b0d23e5b27a71b61158618313f9568e40dffc3fb
Shinobi <86633847+Shinobi7k@users.noreply.github.com>
2023-12-17 23:57:53 +0200

    fixed /poll bug and made it persistant

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,3 +1,5 @@
+import ast
+import aiosqlite
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -39,46 +41,86 @@ class giveawayButton(discord.ui.View):
 class pollButtons(discord.ui.View):
     def __init__(self, *, timeout = None):
         super().__init__(timeout = timeout)
-    @discord.ui.button(label = "Yes", style = discord.ButtonStyle.blurple, emoji = "<:pepeyes:1070834912782987424>")
+    @discord.ui.button(label = "Yes", style = discord.ButtonStyle.blurple, emoji = "<:pepeyes:1070834912782987424>", custom_id = "poll_yes_button")
     async def poll_yes_button(self, interaction: discord.Interaction, button: discord.ui.Button):
-        global upvotes, downvotes, yes_pollers, no_pollers
-        if interaction.user in yes_pollers:
-            upvotes = upvotes - 1
-            yes_pollers.remove(interaction.user)
-            await interaction.response.send_message("Vote removed.", ephemeral = True)
-        elif interaction.user in no_pollers:
-            downvotes = downvotes - 1
-            no_pollers.remove(interaction.user)
-            upvotes = upvotes + 1
-            yes_pollers.append(interaction.user)
-            await interaction.response.send_message("Voted.", ephemeral = True)
-        else:
-            upvotes = upvotes + 1
-            yes_pollers.append(interaction.user)
-            await interaction.response.send_message("Voted.", ephemeral = True)
+        # Fetching poll data...
+        async with aiosqlite.connect("db/polls.db") as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("SELECT * FROM polls WHERE poll_id = ?", (interaction.message.id,))
+                data = await cursor.fetchone()
+                poll_title = data[1]
+                poll_description = data[2]
+                poll_author = data[3]
+                poll_avatar = data[4]
+                upvotes = data[5]
+                downvotes = data[6]
+                upvote_users = ast.literal_eval(data[7])
+                downvote_users = ast.literal_eval(data[8])
+                print("==== Yes ====")
+                print(f"upvote_users {upvote_users}")
+                print(f"downvote_users {downvote_users}")
+
+                if interaction.user.id in upvote_users:
+                    upvotes = upvotes - 1
+                    upvote_users.remove(interaction.user.id)
+                    await cursor.execute("UPDATE polls SET upvotes = ?, upvote_users = ? WHERE poll_id = ?", (upvotes, str(upvote_users), interaction.message.id,))
+                    await interaction.response.send_message("Vote removed.", ephemeral = True)
+                elif interaction.user.id in downvote_users:
+                    downvotes = downvotes - 1
+                    downvote_users.remove(interaction.user.id)
+                    upvotes = upvotes + 1
+                    upvote_users.append(interaction.user.id)
+                    await cursor.execute("UPDATE polls SET upvotes = ?, upvote_users = ?, downvotes = ?, downvote_users = ? WHERE poll_id = ?", (upvotes, str(upvote_users), downvotes, str(downvote_users), interaction.message.id,))
+                    await interaction.response.send_message("Voted.", ephemeral = True)
+                else:
+                    upvotes = upvotes + 1
+                    upvote_users.append(interaction.user.id)
+                    await cursor.execute("UPDATE polls SET upvotes = ?, upvote_users = ? WHERE poll_id = ?", (upvotes, str(upvote_users), interaction.message.id,))
+                    await interaction.response.send_message("Voted.", ephemeral = True)
+                await db.commit()
         emb = discord.Embed(title = poll_title, description = poll_description)
         emb.set_author(name = f"Poll by {poll_author}", icon_url = poll_avatar)
         emb.set_footer(text = f"{upvotes} Yes | {downvotes} No")
         view = pollButtons()
         await interaction.message.edit(embed = emb, view = view)
     #no button
-    @discord.ui.button(label = "No", style = discord.ButtonStyle.blurple, emoji = "<:pepeno:1070834894537773087>")
+    @discord.ui.button(label = "No", style = discord.ButtonStyle.blurple, emoji = "<:pepeno:1070834894537773087>", custom_id = "poll_no_button")
     async def sugg_downvote(self, interaction: discord.Interaction, button: discord.ui.Button):
-        global upvotes, downvotes, no_pollers, yes_pollers
-        if interaction.user in no_pollers:
-            downvotes = downvotes - 1
-            no_pollers.remove(interaction.user)
-            await interaction.response.send_message("Vote removed.", ephemeral = True)
-        elif interaction.user in yes_pollers:
-            upvotes = upvotes - 1
-            yes_pollers.remove(interaction.user)
-            downvotes = downvotes + 1
-            no_pollers.append(interaction.user)
-            await interaction.response.send_message("Voted.", ephemeral = True)
-        else:
-            downvotes = downvotes + 1
-            no_pollers.append(interaction.user)
-            await interaction.response.send_message("Voted.", ephemeral = True)
+         # Fetching poll data...
+        async with aiosqlite.connect("db/polls.db") as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("SELECT * FROM polls WHERE poll_id = ?", (interaction.message.id,))
+                data = await cursor.fetchone()
+                poll_title = data[1]
+                poll_description = data[2]
+                poll_author = data[3]
+                poll_avatar = data[4]
+                upvotes = data[5]
+                downvotes = data[6]
+                upvote_users = ast.literal_eval(data[7])
+                downvote_users = ast.literal_eval(data[8])
+                print("==== No ====")
+                print(f"upvote_users {upvote_users}")
+                print(f"downvote_users {downvote_users}")
+
+                if interaction.user.id in downvote_users:
+                    downvotes = downvotes - 1
+                    downvote_users.remove(interaction.user.id)
+                    await cursor.execute("UPDATE polls SET downvotes = ?, downvote_users = ? WHERE poll_id = ?", (downvotes, str(downvote_users), interaction.message.id,))
+                    await interaction.response.send_message("Vote removed.", ephemeral = True)
+                elif interaction.user.id in upvote_users:
+                    upvotes = upvotes - 1
+                    upvote_users.remove(interaction.user.id)
+                    downvotes = downvotes + 1
+                    downvote_users.append(interaction.user.id)
+                    await cursor.execute("UPDATE polls SET downvotes = ?, downvote_users = ?, upvotes = ?, upvote_users = ? WHERE poll_id = ?", (downvotes, str(downvote_users), upvotes, str(upvote_users), interaction.message.id,))
+                    await interaction.response.send_message("Voted.", ephemeral = True)
+                else:
+                    downvotes = downvotes + 1
+                    downvote_users.append(interaction.user.id)
+                    await cursor.execute("UPDATE polls SET downvotes = ?, downvote_users = ? WHERE poll_id = ?", (downvotes, str(downvote_users), interaction.message.id,))
+                    await interaction.response.send_message("Voted.", ephemeral = True)
+                await db.commit()
         emb = discord.Embed(title = poll_title, description = poll_description)
         emb.set_author(name = f"Poll by {poll_author}", icon_url = poll_avatar)
         emb.set_footer(text = f"{upvotes} Yes | {downvotes} No")
@@ -422,20 +464,17 @@ class Utility(commands.Cog):
     @app_commands.checks.cooldown(1, 10, key = lambda i: (i.user.id))
     @app_commands.checks.has_permissions(manage_messages = True)
     async def poll(self, interaction: discord.Interaction, title: str, description: str):
-        global poll_title, poll_description, poll_author, poll_avatar, upvotes, downvotes, yes_pollers, no_pollers
-        yes_pollers = []
-        no_pollers = []
-        upvotes = 0
-        downvotes = 0
-        poll_title = title
-        poll_description = description
-        poll_author = interaction.user
-        poll_avatar = interaction.user.avatar.url
         view = pollButtons()
-        emb = discord.Embed(title = poll_title, description = poll_description)
-        emb.set_author(name = f"Poll by {poll_author}", icon_url = poll_avatar)
-        emb.set_footer(text = f"{upvotes} Yes | {downvotes} No")
-        await interaction.response.send_message(embed = emb, view = view)
+        emb = discord.Embed(title = title, description = description)
+        emb.set_author(name = f"Poll by {interaction.user.display_name}", icon_url = interaction.user.avatar.url)
+        emb.set_footer(text = f"0 Yes | 0 No")
+        await interaction.response.send_message("Poll Created!", ephemeral=True)
+        poll_msg = await interaction.channel.send(embed = emb, view = view)
+        async with aiosqlite.connect("db/polls.db") as db:
+            async with db.cursor() as cursor:
+                await cursor.execute("CREATE TABLE IF NOT EXISTS polls (poll_id INTEGER, poll_title TEXT, poll_description TEXT, poll_author TEXT, poll_avatar TEXT, upvotes INTEGER, downvotes INTEGER, upvote_users TEXT, downvote_users TEXT)")
+                await cursor.execute("INSERT INTO polls (poll_id, poll_title, poll_description, poll_author, poll_avatar, upvotes, downvotes, upvote_users, downvote_users) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", (poll_msg.id, title, description, interaction.user.display_name, interaction.user.avatar.url, 0, 0, "[]", "[]"))
+                await db.commit()
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Utility(bot))
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import aiohttp
 from discord.ext import commands
 from cogs.ticket import ticket_launcher, main
 from cogs.settings import suggVotes
+from cogs.utility import pollButtons
 from datetime import datetime
 import logging
 import logging.handlers
@@ -39,6 +40,7 @@ class MyBot(commands.Bot):
             self.add_view(ticket_launcher())
             self.add_view(main())
             self.add_view(suggVotes())
+            self.add_view(pollButtons())
             self.added = True
             await bot.change_presence(activity = discord.Game(name = "/help start")) # Setting `Playing` status
         if not os.path.exists("db"): os.makedirs("db") # Create db dir if not there
